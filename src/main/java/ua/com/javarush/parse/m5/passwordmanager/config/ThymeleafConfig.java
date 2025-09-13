19dabff3d85e05f4855a14f20c6cce7eda8546bc
oleksandr-jr <oleksandr.cho@gmail.com>
2025-09-13 14:26:43 +0200

    feat: Re-added ThymeleafConfig for HtmxDialect

---
+++ src/main/java/ua/com/javarush/parse/m5/passwordmanager/config/ThymeleafConfig.java
@@ -0,0 +1,19 @@
+package ua.com.javarush.parse.m5.passwordmanager.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import io.github.wimdeblauwe.htmx.spring.boot.thymeleaf.HtmxDialect;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        templateEngine.addDialect(new HtmxDialect());
+        return templateEngine;
+    }
+}
