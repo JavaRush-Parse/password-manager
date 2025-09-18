3e19a15c8de576621299d6654ca970c10a72be37
oleksandr-jr <oleksandr.cho@gmail.com>
2025-09-18 18:32:36 +0200

    Login and register pages implementation

---
+++ src/main/java/ua/com/javarush/parse/m5/passwordmanager/controller/web/RegisterController.java
@@ -0,0 +1,13 @@
+package ua.com.javarush.parse.m5.passwordmanager.controller.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegisterController {
+
+  @GetMapping("/register")
+  public String register() {
+    return "register";
+  }
+}
